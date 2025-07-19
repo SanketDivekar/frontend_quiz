f06e76ebc4fecb470ded9c6fed017802e59bc794
SanketDivekar <sktdivekar@gmail.com>
2025-07-20 00:14:45 +0530

    Added one extra variable to render background color for subject card image.

--- lib/reusable_widgets/subject_card.dart
+++ lib/reusable_widgets/subject_card.dart
@@ -7,10 +7,12 @@ class SubjectCard extends StatelessWidget {
     super.key,
     required this.imageName,
     required this.subjectName,
+    required this.backgroundColor,
   });
 
   final String imageName;
   final String subjectName;
+  final Color backgroundColor;
 
   @override
   Widget build(BuildContext context) {
@@ -18,51 +20,59 @@ class SubjectCard extends StatelessWidget {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => QuestionsPage(subjectName: selectedSubjectName),
+          builder:
+              (context) => QuestionsPage(
+                subjectName: selectedSubjectName,
+                subjectImage: imageName,
+                subjectImageBgColor: backgroundColor,
+              ),
         ),
       );
     }
 
-    return GestureDetector(
-      child: Container(
-        margin: EdgeInsets.all(15.0),
-        height: 80.0,
-        width: 450.0,
-        decoration: BoxDecoration(
-          color: Color(0xff3C4C67),
-          borderRadius: BorderRadius.circular(12.0),
-        ),
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: const Color(0xFFFDEDD6),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                height: 60,
-                width: 55,
-                child: Image.asset(
-                  imageName,
-                  // fit: BoxFit.fill,
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        child: Container(
+          margin: EdgeInsets.all(15.0),
+          height: 75.0,
+          width: 400.0,
+          decoration: BoxDecoration(
+            color: Color(0xff3C4C67),
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: backgroundColor,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  height: 60,
+                  width: 55,
+                  child: Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: Image.asset(imageName),
+                  ),
                 ),
               ),
-            ),
-            Text(
-              subjectName,
-              style: GoogleFonts.poppins(
-                color: Colors.white,
-                fontSize: 20.0,
-                fontWeight: FontWeight.w600,
+              Text(
+                subjectName,
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
+        onTap: () {
+          onTapSubjectTile(subjectName);
+        },
       ),
-      onTap: () {
-        onTapSubjectTile(subjectName);
-      },
     );
   }
 }
