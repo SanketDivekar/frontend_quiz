d58119ba5c71d69ef9e55e7a4035c431612880ef
SanketDivekar <sktdivekar@gmail.com>
2025-07-22 22:06:48 +0530

    Added logic to iterate the questions and answers and logic for progress indicator.

--- lib/questions_page.dart
+++ lib/questions_page.dart
@@ -4,6 +4,7 @@ import 'package:frontend_quiz/data/css_questions.dart';
 import 'package:frontend_quiz/data/html_questions.dart';
 import 'package:frontend_quiz/data/javascript_questions.dart';
 import 'package:frontend_quiz/models/quiz_question_model.dart';
+import 'package:frontend_quiz/reusable_widgets/answer_button.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsPage extends StatefulWidget {
@@ -22,7 +23,21 @@ class QuestionsPage extends StatefulWidget {
 }
 
 class _QuestionsPageState extends State<QuestionsPage> {
-  var currentQuestionIndex = 0;
+  int currentQuestionIndex = 0;
+  var progressIndicatorValue = 0.0;
+  List<String> userSelectedAnswers = [];
+
+  void onClickAnswer(String answer) {
+    userSelectedAnswers.add(answer);
+  }
+
+  void onSubmitAnswer() {
+    setState(() {
+      currentQuestionIndex++;
+      progressIndicatorValue += 0.1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final QuizQuestionModel currentQuestion;
@@ -36,130 +51,125 @@ class _QuestionsPageState extends State<QuestionsPage> {
       currentQuestion = javascriptQuestion[currentQuestionIndex];
     }
     return Scaffold(
-      backgroundColor: const Color(0xff313E51),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 170.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/assets/background_image.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 155.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: widget.subjectImageBgColor,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    height: 60,
+                    width: 55,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: Image.asset(widget.subjectImage),
+                    ),
+                  ),
+                  SizedBox(width: 8.0),
+                  Text(
+                    widget.subjectName,
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: widget.subjectImageBgColor,
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  height: 60,
-                  width: 55,
-                  child: Padding(
-                    padding: const EdgeInsets.all(6.0),
-                    child: Image.asset(widget.subjectImage),
-                  ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Question ${currentQuestionIndex + 1} of 10',
+                      style: GoogleFonts.poppins(
+                        color: Color(0xffA2B1C8),
+                        fontSize: 16.0,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    SizedBox(height: 30.0),
+                    Text(
+                      softWrap: true,
+                      currentQuestion.question,
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 250.0),
+                    SizedBox(
+                      height: 10.0,
+                      width: 350.0,
+                      child: LinearProgressIndicator(
+                        borderRadius: BorderRadius.circular(8.0),
+                        value: progressIndicatorValue,
+                        backgroundColor: const Color(0xff3C4C67),
+                        color: const Color(0xffA62AF2),
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(width: 8.0),
-                Text(
-                  widget.subjectName,
-                  style: GoogleFonts.poppins(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w600,
-                  ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ...currentQuestion.getShuffledAnswers().map((answer) {
+                      return AnswerButton(
+                        answerText: answer,
+                        onClick: () {
+                          onClickAnswer(answer);
+                        },
+                      );
+                    }),
+                    SizedBox(height: 10.0),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 25,
+                          horizontal: 145,
+                        ),
+                        backgroundColor: const Color(0xffA62AF2),
+                        foregroundColor: const Color(0xFF20082F),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(14.0),
+                        ),
+                      ),
+                      onPressed: () {
+                        onSubmitAnswer();
+                      },
+                      child: Text(
+                        'Submit answer',
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Question 6 of 10',
-                    style: GoogleFonts.poppins(
-                      color: Color(0xffA2B1C8),
-                      fontSize: 16.0,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                  SizedBox(height: 30.0),
-                  Text(
-                    softWrap: true,
-                    currentQuestion.question,
-                    style: GoogleFonts.poppins(
-                      color: Colors.white,
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  SizedBox(height: 250.0),
-                  SizedBox(
-                    height: 10.0,
-                    width: 350.0,
-                    child: LinearProgressIndicator(
-                      borderRadius: BorderRadius.circular(8.0),
-                      value: 0.6,
-                      backgroundColor: const Color(0xff3C4C67),
-                      color: const Color(0xffA62AF2),
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                // mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    margin: EdgeInsets.all(15.0),
-                    height: 75.0,
-                    width: 400.0,
-                    decoration: BoxDecoration(
-                      color: Color(0xff3C4C67),
-                      borderRadius: BorderRadius.circular(16.0),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(15.0),
-                    height: 75.0,
-                    width: 400.0,
-                    decoration: BoxDecoration(
-                      color: Color(0xff3C4C67),
-                      borderRadius: BorderRadius.circular(16.0),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(15.0),
-                    height: 75.0,
-                    width: 400.0,
-                    decoration: BoxDecoration(
-                      color: Color(0xff3C4C67),
-                      borderRadius: BorderRadius.circular(16.0),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(15.0),
-                    height: 75.0,
-                    width: 400.0,
-                    decoration: BoxDecoration(
-                      color: Color(0xff3C4C67),
-                      borderRadius: BorderRadius.circular(16.0),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(15.0),
-                    height: 75.0,
-                    width: 400.0,
-                    decoration: BoxDecoration(
-                      color: Color(0xffA62AF2),
-                      borderRadius: BorderRadius.circular(16.0),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
