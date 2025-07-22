2540e9f857c5be59306fc851e125b30e61e308da
SanketDivekar <sktdivekar@gmail.com>
2025-07-22 22:05:43 +0530

    Added background image to home screen.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,78 +7,85 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff313E51),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  RichText(
-                    text: TextSpan(
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: 'Welcome to the \n',
-                          style: GoogleFonts.poppins(
-                            color: Colors.white,
-                            fontSize: 50.0,
-                            fontWeight: FontWeight.w200,
-                          ),
-                        ),
-                        TextSpan(
-                          text: 'Frontend Quiz!',
-                          style: GoogleFonts.poppins(
-                            color: Colors.white,
-                            fontSize: 55.0,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 30.0),
-                  Text(
-                    'Pick a subject to get started',
-                    style: GoogleFonts.poppins(
-                      color: Color(0xffA2B1C8),
-                      fontSize: 20.0,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SubjectCard(
-                    imageName: 'lib/assets/html_image.png',
-                    subjectName: 'HTML',
-                    backgroundColor: const Color(0xFFFDEDD6),
-                  ),
-                  SubjectCard(
-                    imageName: 'lib/assets/css_image.png',
-                    subjectName: 'CSS',
-                    backgroundColor: const Color(0xFFDAFFDA),
-                  ),
-                  SubjectCard(
-                    imageName: 'lib/assets/javascript_image.png',
-                    subjectName: 'Javascript',
-                    backgroundColor: const Color(0xFFCEEAFF),
-                  ),
-                  SubjectCard(
-                    imageName: 'lib/assets/accessibility_image.png',
-                    subjectName: 'Accessibility',
-                    backgroundColor: const Color(0xFFFCEBFF),
-                  ),
-                ],
-              ),
-            ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/assets/background_image.png'),
+            fit: BoxFit.cover,
           ),
-        ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Welcome to the \n',
+                            style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontSize: 50.0,
+                              fontWeight: FontWeight.w200,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'Frontend Quiz!',
+                            style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontSize: 55.0,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 30.0),
+                    Text(
+                      'Pick a subject to get started',
+                      style: GoogleFonts.poppins(
+                        color: Color(0xffA2B1C8),
+                        fontSize: 20.0,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SubjectCard(
+                      imageName: 'lib/assets/html_image.png',
+                      subjectName: 'HTML',
+                      backgroundColor: const Color(0xFFFDEDD6),
+                    ),
+                    SubjectCard(
+                      imageName: 'lib/assets/css_image.png',
+                      subjectName: 'CSS',
+                      backgroundColor: const Color(0xFFDAFFDA),
+                    ),
+                    SubjectCard(
+                      imageName: 'lib/assets/javascript_image.png',
+                      subjectName: 'Javascript',
+                      backgroundColor: const Color(0xFFCEEAFF),
+                    ),
+                    SubjectCard(
+                      imageName: 'lib/assets/accessibility_image.png',
+                      subjectName: 'Accessibility',
+                      backgroundColor: const Color(0xFFFCEBFF),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
